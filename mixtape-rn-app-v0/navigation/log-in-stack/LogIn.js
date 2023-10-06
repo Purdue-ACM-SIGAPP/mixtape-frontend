df5696a01d28f7538fc03d0564809e091aaa0802
Sam <thesamloop@gmail.com>
2023-10-05 20:34:29 -0400

    complete login page (minus icons)

--- mixtape-rn-app-v0/navigation/log-in-stack/LogIn.js
+++ mixtape-rn-app-v0/navigation/log-in-stack/LogIn.js
@@ -1,14 +1,16 @@
 import * as React from "react";
-import { Button, TextInput, View } from "react-native";
-
-
-
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function LogIn() {
-
-    const [phoneNum, onChangePhoneNum] = React.useState('Phone Number');
-    const [password, onChangePassword] = React.useState('Password');
-
+  const [phoneNum, setPhoneNum] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
   return (
     <>
@@ -17,19 +19,86 @@ export default function LogIn() {
           flex: 1,
           justifyContent: "center",
           alignItems: "center",
+          backgroundColor: "#14151E",
         }}
       >
-        <TextInput
-        value={phoneNum}></TextInput>
-        <TextInput
-        value={password}></TextInput>
 
-        <Button
-        title="Log In"
-        color="#19A7CE"
-      />
+        <Image
+        style={styles.mixtapeLogo}
+        source={{
+          uri: 'https://i.ibb.co/5xqB3BV/mixtape.png',
+        }} />
+
+
+        <TextInput
+          placeholder="&#128241; Phone Number"
+          value={phoneNum}
+          onChangeText={(text) => setPhoneNum(text)}
+          style={styles.textInput}
+        />
         
+
+        <TextInput
+          placeholder="&#128274; Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styles.textInput}
+        />
+
+        <Pressable style={styles.loginButton}>
+          <Text style={styles.loginText}>Log In</Text>
+        </Pressable>
+
+        <Text>
+          <Pressable>
+            <Text style={[styles.textOptions, {marginRight: 100,}, ]}>Forgot Password?</Text>
+          </Pressable>
+
+          <Pressable>
+            <Text style={[styles.textOptions, ]}>Create Account</Text>
+          </Pressable>
+        </Text>
       </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  loginButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    backgroundColor: "#19A7CE",
+    width: 250,
+    marginTop: 20,
+  },
+  loginText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+  textInput: {
+    height: 40,
+    width: 300,
+    borderColor: "#19A7CE",
+    borderWidth: 2.5,
+    marginTop: 20,
+    backgroundColor: "#333333",
+    textAlign: "center",
+    color: "#C7C6C6",
+  },
+  textOptions: {
+    color: "white",
+    marginTop: 40,
+    textDecorationLine: 'underline',
+  },
+  mixtapeLogo: {
+    width: "250px",
+    height: "150px",
+    marginBottom: 20,
+  }
+});
