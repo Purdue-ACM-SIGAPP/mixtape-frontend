24bd8952f3bdbd820a4baeb91b94c1be082743ae
colin wu <colinwu0403@gmail.com>
2023-11-02 21:16:27 -0400

    Added Tailwind CSS to React Native

--- mixtape-rn-app-v0/App.js
+++ mixtape-rn-app-v0/App.js
@@ -1,36 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import * as React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NativeWindStyleSheet } from "nativewind";
 
-import LogIn from './navigation/log-in-stack/LogIn';
-import Home from './navigation/main-stack/Home';
+import LogIn from "./navigation/log-in-stack/LogIn";
+import Home from "./navigation/main-stack/Home";
 
-
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
-
   const Stack = createStackNavigator();
 
   return (
     <NavigationContainer>
-    <Stack.Navigator>
-    <Stack.Screen name="LogIn" component={LogIn} />
+      <Stack.Navigator>
+        <Stack.Screen name="LogIn" component={LogIn} />
 
-    <Stack.Screen name="Home" component={Home} />
-    </Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
-
--- mixtape-rn-app-v0/components/SubmitButton.js
+++ mixtape-rn-app-v0/components/SubmitButton.js
@@ -16,24 +16,27 @@ const SubmitButton = ({ initialText, updatedText, onPress }) => {
   };
 
   return (
-    <Pressable style={styles.button} onPress={handlePress}>
-      <Text style={styles.buttonText}>{buttonText}</Text>
+    <Pressable
+      className="bg-secondary py-[10px] px-[50px] rounded-[7px]"
+      onPress={handlePress}
+    >
+      <Text className="text-ivory text-[18px]">{buttonText}</Text>
     </Pressable>
   );
 };
 
-// The styles are customizable
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: "#19A7CE",
-    paddingVertical: 10,
-    paddingHorizontal: 50,
-    borderRadius: 7,
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 18,
-  },
-});
+// Stylesheet if not using TailwindCSS
+// const styles = StyleSheet.create({
+//   button: {
+//     backgroundColor: "#19A7CE",
+//     paddingVertical: 10,
+//     paddingHorizontal: 50,
+//     borderRadius: 7,
+//   },
+//   buttonText: {
+//     color: "white",
+//     fontSize: 18,
+//   },
+// });
 
 export default SubmitButton;
