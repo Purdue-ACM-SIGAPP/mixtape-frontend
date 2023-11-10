7d014133dd94d97e67d5c96d0cbae18cffe2636f
Sam <thesamloop@gmail.com>
2023-11-09 20:09:00 -0500

    implementing drag&drop pt 1

--- mixtape-rn-app-v0/components/SongComponent.js
+++ mixtape-rn-app-v0/components/SongComponent.js
@@ -1,5 +1,12 @@
-import React from "react";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useRef, useState } from "react";
+import {
+  Animated,
+  Image,
+  PanResponder,
+  StyleSheet,
+  Text,
+  TouchableOpacity
+} from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 
 const SongComponent = ({ title, image }) => {
@@ -10,8 +17,51 @@ const SongComponent = ({ title, image }) => {
     image = "https://i.ibb.co/5xqB3BV/mixtape.png";
   }
 
+  // Create a ref to store the position of the card
+  const position = useRef(new Animated.ValueXY()).current;
+
+  // State to track if the card is being dragged
+  const [dragging, setDragging] = useState(false);
+
+  // Create a pan responder to handle touch events
+  const panResponder = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        // When touch gesture starts,
+        //set dragging to true
+        setDragging(true);
+      },
+      onPanResponderMove: Animated.event(
+        [
+          null,
+          {
+            dy: position.y,
+          },
+        ],
+        { useNativeDriver: false }
+      ),
+      onPanResponderRelease: () => {
+        // When touch gesture is released,
+        //set dragging to false
+        setDragging(false);
+      },
+    })
+  ).current;
+
+
   return (
-    <View style={styles.container}>
+    <Animated.View
+      style={[
+        styles.container,
+        {
+          transform: position.getTranslateTransform(),
+          opacity: dragging ? 0.8 : 1,
+        },
+      ]}
+      {...panResponder.panHandlers}
+    >
       <Image
         style={styles.imageHolder}
         source={{
@@ -22,9 +72,15 @@ const SongComponent = ({ title, image }) => {
       <Text style={styles.songTitle}>{title}</Text>
 
       <TouchableOpacity>
-        <Icon name="menu-outline" size={30} color="#0094FF" />
+        <Icon name="menu-outline" size={30} color="#0094FF" style={[
+          {
+            transform: position.getTranslateTransform(),
+            opacity: dragging ? 0.8 : 1,
+          },
+        ]}
+        {...panResponder.panHandlers} />
       </TouchableOpacity>
-    </View>
+    </Animated.View>
   );
 };
 
