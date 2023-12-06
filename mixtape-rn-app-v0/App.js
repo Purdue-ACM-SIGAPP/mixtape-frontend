1f462ae0e72a7b6de582afc33113d8c5a6d584bc
AndrewZacharyLiu <87785681+AndrewZacharyLiu@users.noreply.github.com>
2023-12-05 20:01:00 -0500

    Finished Page

--- mixtape-rn-app-v0/App.js
+++ mixtape-rn-app-v0/App.js
@@ -3,11 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { NativeWindStyleSheet } from 'nativewind';
 import LogIn from './navigation/log-in-stack/LogIn';
 import Home from './navigation/main-stack/Home';
+import Verification from './navigation/log-in-stack/Verification';
 
-
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
 
@@ -16,7 +19,7 @@ export default function App() {
   return (
     <NavigationContainer>
     <Stack.Navigator>
-    
+      <Stack.Screen name="Verification" component={Verification} />
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="LogIn" component={LogIn} />
     </Stack.Navigator>
---
+++ mixtape-rn-app-v0/navigation/log-in-stack/Verification.js
@@ -0,0 +1,152 @@
+import * as React from "react";
+import { useState, useRef } from "react";
+import {
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import Icon from "react-native-vector-icons/Feather";
+import SubmitButton from "../../components/SubmitButton";
+import { TouchableOpacity, Linking } from 'react-native';
+
+
+export default function Verification() {
+  const [isClicked, setIsClicked] = React.useState(false);
+  const [checkValid, setCheckValid] = React.useState(true);
+  const [code, setCode] = React.useState("");
+
+  const backToLogin = () => {
+    return (
+      // need to go back to login page
+      console.log("pressed back")
+    );
+  }
+
+  const tryAgain = () => {
+      // Handle the link press action here
+      Linking.openURL('https://youtube.com');
+      console.log("pressed back");
+    };
+  const VerificationCode = (code) => {
+    return false;
+  };
+  
+
+  return (
+    <>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#14151E",
+        }}
+      >
+        
+        <Text style={styles.ForgotPassword}>Enter Verification Code</Text>
+        <Text style={styles.SubheaderGreen}>Verification sent to XXX-XXX-XXXX</Text>
+        <View>
+
+          <View style={styles.inputContainer}>
+            <Icon name="lock" size={20} color="#C7C6C6" style={styles.icon} />
+            <TextInput
+              placeholder="Verification Code"
+              value={code}
+              onChangeText={(text) => setCode(text)}
+              style={styles.textInput}
+              maxLength={10}
+            />
+          </View>
+          <View>
+            { isClicked && checkValid ? (<Text style={styles.errorText}>Incorrect. Try again.</Text>) : 
+                                        (<Text style={styles.errorText}>&nbsp;</Text>)}
+            </View>
+        </View>
+        <Text style={styles.Subheader}>
+        Didn't receive it?&nbsp;&nbsp;
+        <Text style={[styles.Subheader, styles.underline]} onPress={tryAgain}>Resend</Text>
+        </Text>
+
+        <View style={styles.addSpacing}></View>
+
+        <SubmitButton initialText="Submit" updatedText="Submit" onPress={() => VerificationCode(code)}/>
+        <Text style={[styles.SubheaderBottom, styles.underline]} onPress={tryAgain}>Back to Sign Up</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  textInput: {
+    height: 30,
+    width: 300,
+    backgroundColor: "#333333",
+    textAlign: "left",
+    color: "#C7C6C6",
+    flex: 1,
+  },
+  underline: {
+    textDecorationLine: 'underline', 
+  },
+  textOptions: {
+    color: "white",
+    marginTop: 10,
+    textDecorationLine: "underline",
+    justifyContent: "center",
+    
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#333333",
+    height: 40,
+    width: 300,
+    borderColor: "#19A7CE",
+    borderWidth: 2.5,
+    borderRadius: 7,    
+  },
+  icon: {
+    marginRight: 10,
+    paddingLeft: 5,
+  },
+  ForgotPassword: {
+    color: "white",
+    fontSize: 35,
+    marginBottom: 20,
+  },
+  SubheaderGreen: {
+    color: "#60DE83",
+    fontSize: 15,
+    marginBottom: 80,
+  },
+  Subheader: {
+    color: "white",
+    fontSize: 15,
+    marginBottom: 80,
+  },
+  SubheaderBottom: {
+    color: "white",
+    fontSize: 15,
+    marginBottom: 40,
+    position: "absolute",
+    bottom: 0,
+  },
+  errorText: {
+    display: 'flex',
+    color: 'red',
+    paddingBottom: 3,
+    justifyContent: "center",
+  },
+  addSpacing: {
+    padding: 20,
+  },
+  bottomSection: {
+    marginTop: 200,
+  },
+  TopSection: {
+    marginBottom: 200,
+  },
+});
