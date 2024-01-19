3c84dc336ec91b51ba47a57bbaad1ac7a6e33bdb
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-01-18 20:58:48 -0500

    Fixed importing error

--- mixtape-rn-app-v0/assets/index.js
+++ mixtape-rn-app-v0/assets/index.js
@@ -2,5 +2,6 @@ import dancing_meme from "./dancing_meme.jpg";
 import spotify from "./spotify.png";
 import github from "./github.png";
 import applemusic from "./applemusic.png";
+import BackSprite from "./back.png";
 
-export { dancing_meme, applemusic, spotify, github };
+export { dancing_meme, applemusic, spotify, github, BackSprite };
