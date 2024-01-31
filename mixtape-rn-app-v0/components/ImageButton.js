bfde6b401653c7acd3333ab87efe4949c8dcf163
alexkobus <70383566+alexkobus@users.noreply.github.com>
2024-01-30 20:06:18 -0500

    dumb "fix" to clickable padding problem

--- mixtape-rn-app-v0/components/ImageButton.js
+++ mixtape-rn-app-v0/components/ImageButton.js
@@ -14,15 +14,17 @@ export const ImageButton = ({ imageName, onPress, imageText }) => {
   };
 
   return (
-    <Pressable onPress={handlePress}>
+    <Pressable onPress={()=>{}}>
       <View className="justify-center items-center mb-2 mr-10 ml-10 w-full h-full">
-        <Image
-          className="xs:w-[120px] xs:h-[120px] sm:w-[180px] sm:h-[180px]"
-          source={imageName}
-        />
-        <Text className="text-white mt-2 text-[20px] text-center">
-          {imageText}
-        </Text>
+        <Pressable onPress={handlePress}>
+          <Image
+            className="xs:w-[120px] xs:h-[120px] sm:w-[180px] sm:h-[180px]"
+            source={imageName}
+          />
+          <Text className="text-white mt-2 text-[20px] text-center">
+            {imageText}
+          </Text>
+        </Pressable>
       </View>
     </Pressable>
   );
