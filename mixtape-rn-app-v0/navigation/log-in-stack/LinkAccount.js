750872b45719d72ef4e6fbeab4a21a25173b3672
Colin Wu <85907102+ColinWu0403@users.noreply.github.com>
2024-02-06 19:34:59 -0500

    Update App.js

--- mixtape-rn-app-v0/navigation/log-in-stack/LinkAccount.js
+++ mixtape-rn-app-v0/navigation/log-in-stack/LinkAccount.js
@@ -3,10 +3,17 @@ import { Image, View, Text, StyleSheet } from "react-native";
 import { spotify } from "../../assets";
 import { applemusic } from "../../assets";
 import { ImageButton } from "../../components/ImageButton";
+import { AddFriendButton } from "../../components/AddFriendButton";
 
 export default function LinkAccount() {
   return (
     <View className="flex-1 justify-center items-center bg-tertiary">
+      <AddFriendButton
+        onPress={() => {
+          console.log("Add Friend button pressed");
+        }}
+        imageText={"Add Friend"}
+      />
       <Text className="mt-8 text-white xs:text-xl sm:text-3xl mb-10">
         Choose your music streaming service
       </Text>
